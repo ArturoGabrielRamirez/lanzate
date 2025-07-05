1793df36055a25d9d9a275d01fddcf5574277982
ArturoGabrielRamirez <jiamafox@gmail.com>
2025-07-05 06:33:39 -0300

    Restoring work

---
+++ app/actions/handleGoogleLogIn.ts
@@ -0,0 +1,52 @@
+'use server'
+
+import { createClient } from '@/utils/supabase/server-props'
+import { revalidatePath } from 'next/cache'
+import { redirect } from 'next/navigation'
+
+const getURL = () => {
+    let url =
+        process?.env?.NEXTAUTH_URL ??
+        process?.env?.NEXT_PUBLIC_VERCEL_URL ??
+        'http://localhost:3000/'
+
+    url = url.startsWith('http') ? url : `https://${url}`
+
+    url = url.endsWith('/') ? url : `${url}/`
+    return url
+}
+
+export async function handleGoogleLogIn() {
+    const supabase = await createClient()
+    const redirectUrl = `${getURL()}auth/callback`;
+
+    const { data, error } = await supabase.auth.signInWithOAuth({
+        provider: 'google',
+        options: {
+            redirectTo: redirectUrl,
+            queryParams: {
+                access_type: 'offline',
+                prompt: 'consent',
+            },
+        },
+    })
+
+    if (error) {
+        console.log(error)
+        return;
+    }
+
+    if (data?.url) {
+        redirect(data.url);
+    }
+
+    if (data) {
+        const { data: { user } } = await supabase.auth.getUser()
+        if (user) {
+            const { id, email, created_at } = user
+            console.log(id, email, created_at)
+            revalidatePath('/', 'layout')
+            redirect('/')
+        }
+    }
+}
