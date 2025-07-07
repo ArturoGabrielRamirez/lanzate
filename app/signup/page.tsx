6fdf094a5716b8224297a1e2632e7c4d950ebc1a
ArturoGabrielRamirez <jiamafox@gmail.com>
2025-07-07 14:03:12 -0300

    Refactor SignupPage component by removing unused imports and deleting GridForms component

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,27 +1,7 @@
-import Title from '@/features/layout/components/title'
-import GridForms from '@/components/Visuals/GridFroms'
-import { handleSignup } from '../../features/auth/actions/handleSignUp'
-import Divider from '@/components/Visuals/Divider'
-import Form from '@/features/layout/components/form'
-import InputField from '@/features/layout/components/input'
-/* import { signupSchema } from '@/features/auth/schemas/signupSchema' */
-import { yupResolver } from '@hookform/resolvers/yup'
-import * as yup from 'yup'
 import SignupForm from '@/features/auth/components/form-signup'
-
 
 export default function SignupPage() {
     return (
         <SignupForm />
-        /*  <Form
-           formAction={handleSignup}
-           resolver={yupResolver(signupSchema)}
-           contentButton="Sign up"
-           successRedirect="/check-email"
-           successMessage="Te registraste con éxito. Revisa tu email."
-         >
-           <InputField name="email" label="Email" type="email" />
-           <InputField name="password" label="Password" type="password" />
-         </Form> */
     )
 }
