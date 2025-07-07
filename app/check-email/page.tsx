25daf8ebba73763a9898531065912dbf9b85fafb
ArturoGabrielRamirez <jiamafox@gmail.com>
2025-07-07 14:06:38 -0300

    Update CheckEmail component to use a custom background color class

--- app/check-email/page.tsx
+++ app/check-email/page.tsx
@@ -1,6 +1,6 @@
 export default function CheckEmail() {
     return (
-        <div className="p-8 text-center grow bg-gray-800 text-white flex flex-col items-center justify-center">
+        <div className="p-8 text-center grow --color-background text-white flex flex-col items-center justify-center">
             <h2 className="text-2xl font-bold mb-4">Please check your email for the confirmation link.</h2>
             <p>We have sent you a confirmation link. Please verify it to complete your registration.</p>
         </div>
