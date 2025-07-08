5a95648ffcc801c7b4d12c475195875991a5390c
Horacio Gutierrez <horacio.estevez@gmail.com>
2025-07-07 22:20:51 -0300

    Refactor Store Card Component and Update Stores Page Styling
    - Replaced the store card's structure with a new Card component for improved styling and layout consistency.
    - Enhanced the store card to include additional information such as description and creation date, improving user experience.
    - Updated the Stores page to adjust the border opacity of the store card for better visual clarity.
    - Ensured all changes adhere to responsiveness and accessibility standards.

--- features/stores/components/store-card.tsx
+++ features/stores/components/store-card.tsx
@@ -1,5 +1,6 @@
 "use client"
 
+import { Card, CardContent, CardFooter, CardHeader, CardTitle } from "@/components/ui/card"
 import { Store } from "@/prisma/generated/prisma"
 import { useRouter } from "next/navigation"
 
@@ -16,9 +17,28 @@ function StoreCard({ store }: Props) {
     }
 
     return (
-        <article key={store.id} className="border-2 border-secondary rounded-md p-4 hover:scale-105 transition-all bg-accent text-accent-foreground cursor-pointer" onClick={handleClick}>
-            <h2 className="text-2xl font-bold">{store.name}</h2>
-        </article>
+        <Card className="hover:scale-105 transition-all cursor-pointer hover:bg-primary hover:text-primary-foreground" onClick={handleClick}>
+            <CardHeader>
+                <CardTitle>
+                    <h2 className="text-2xl font-bold">{store.name}</h2>
+                </CardTitle>
+            </CardHeader>
+            <CardContent>
+                <p className="text-sm text-muted-foreground">{store.description || "No description"}</p>
+            </CardContent>
+            <CardFooter className="justify-between">
+                <p className="text-sm text-muted-foreground">
+                    created at: {store.created_at.toLocaleDateString()}
+                </p>
+                <p className="text-sm text-muted-foreground">
+                    0 products
+                </p>
+            </CardFooter>
+            {/* <article key={store.id} className="border-2 border-secondary rounded-md p-4 hover:scale-105 transition-all bg-accent text-accent-foreground cursor-pointer" onClick={handleClick}>
+
+                <h2 className="text-2xl font-bold">{store.name}</h2>
+            </article> */}
+        </Card>
     )
 }
 
