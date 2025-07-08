bd46a010431d17148c6524b7144d7d4b282dca65
Horacio Gutierrez <horacio.estevez@gmail.com>
2025-07-08 03:58:18 -0300

    Refactor NotFound Component for Improved Layout and User Experience
    - Updated the NotFound component to include an OctagonX icon for better visual representation of the error state.
    - Enhanced the layout by using a section element and adjusting text for clarity and responsiveness.
    - Ensured all changes adhere to accessibility standards.

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,13 +1,17 @@
+import { OctagonX } from 'lucide-react'
 import Link from 'next/link'
 
 export default function NotFound() {
     return (
-        <div className='flex flex-col items-center justify-center h-screen bg-gray-800'>
-            <h1 className='text-4xl font-bold text-red-600'>Oops...nothing to see here.</h1>
-            <p className='mt-4 text-lg text-gray-700'>Something went wrong.</p>
+        <section className='grow flex flex-col items-center justify-center gap-4'>
+            <OctagonX className='text-destructive' size={50} />
+            <div className='flex flex-col items-center justify-center gap-2'>
+                <p className='text-4xl font-bold'>Oops...404 nothing to see here.</p>
+                <p className='text-lg'>The page you are looking for does not exist.</p>
+            </div>
             <Link href='/' className='mt-6 text-blue-500 hover:underline'>
                 Go back to Home
             </Link>
-        </div>
+        </section>
     )
 }
