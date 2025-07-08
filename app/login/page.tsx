3c6c3085259e651cc571d41d474ece2ed7e2251b
Horacio Gutierrez <horacio.estevez@gmail.com>
2025-07-07 22:36:45 -0300

    Refactor Login Page Layout and Remove Divider Component
    - Updated the LoginPage component to enhance layout and responsiveness, replacing the previous structure with a more flexible design using Tailwind CSS.
    - Removed the Divider component as it was no longer necessary for the layout.
    - Improved accessibility and styling of the ResetPasswordLink component.
    - Ensured all changes adhere to responsiveness and accessibility standards.

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,5 +1,4 @@
 import { Label } from '@/components/ui/label'
-import Divider from '@/components/Visuals/Divider'
 import Form from '@/features/layout/components/form'
 import Title from '@/features/layout/components/title'
 import ResetPasswordLink from '@/features/auth/components/reset-password-link'
@@ -7,19 +6,21 @@ import { handleGoogleLogIn } from '../../features/auth/actions/handleGoogleLogIn
 import LoginForm from '@/features/auth/components/form-login'
 
 export default async function LoginPage() {
-    const pageLayoutClasses = `px-10 grow flex items-center grid grid-rows-[1fr_1fr] grid-cols-[1fr] sm:grid-rows-[40px_1fr] sm:grid-cols-[1fr_1px_1fr] justify-center bg-white dark:bg-gray-800 sm:gap-x-20 sm:p-8`
-    const titleClasses = `row-start-1`
-    const formOtherClasses = `md:pt-2 sm:row-start-2 sm:col-start-3 sm:min-w-full flex justify-center`
+
 
     return (
-        <div className={pageLayoutClasses}>
-            <Title title='Log In' className={titleClasses} />
-            <LoginForm />
-            <Divider className='h-2/3' />
-            <Form formAction={handleGoogleLogIn} className={formOtherClasses} contentButton='Google'>
-                <Label htmlFor='google' className='pl-2'>Or login with:</Label>
-            </Form>
-            <ResetPasswordLink />
+        <div className="p-4 grow flex flex-col">
+            <Title title='Log In' />
+            <div className='flex flex-col lg:flex-row gap-4 w-full max-w-4xl mx-auto grow items-center'>
+                <div className='w-full lg:pr-10'>
+                    <LoginForm />
+                    <ResetPasswordLink />
+                </div>
+                <div className='lg:w-1 lg:h-100 w-full h-1 bg-primary/50'></div>
+                <Form formAction={handleGoogleLogIn} className={"w-full grid place-content-left lg:pl-10"} contentButton='Google'>
+                    <Label htmlFor='google' className='mb-2 w-full text-center justify-center'>Or login with:</Label>
+                </Form>
+            </div>
         </div>
     )
 }
