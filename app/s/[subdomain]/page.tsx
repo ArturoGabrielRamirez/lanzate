79832bc42efd22a1b30d9b416b8bb1fb80a64bf7
Horacio Gutierrez <horacio.estevez@gmail.com>
2025-07-09 22:05:25 -0300

    Add Title component to StorePage and adjust layout structure
    - Integrated Title component to display a header for the product section on StorePage.
    - Modified the layout structure to enhance visual hierarchy and organization of product listings.
    - Maintained responsiveness and accessibility standards in the updated design.

--- app/s/[subdomain]/page.tsx
+++ app/s/[subdomain]/page.tsx
@@ -1,5 +1,6 @@
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+import { Title } from "@/features/layout/components";
 import { getStoreWithProducts } from "@/features/subdomain/actions/getStoreWithProducts";
 import { ShoppingCart } from "lucide-react";
 
@@ -17,24 +18,27 @@ export default async function StorePage({ params }: Props) {
     }
 
     return (
-        <section className="grid grid-cols-[repeat(auto-fill,minmax(min(200px,100%),1fr))] gap-4 p-4">
-            {storeData.products.map((product) => (
-                <Card key={product.id} className="aspect-9/12 bg-accent hover:scale-105 transition-all cursor-pointer">
-                    <CardHeader>
-                        <CardTitle>{product.name}</CardTitle>
-                        <CardDescription className="line-clamp-2">{product.description || "No description available for this product"}</CardDescription>
-                    </CardHeader>
-                    <CardContent className="grow">
-                        <img src="https://api.dicebear.com/9.x/icons/svg?seed=boxes" alt="Product Image" className="rounded-md" />
-                    </CardContent>
-                    <CardFooter className="flex justify-between items-center">
-                        <p>${product.price}</p>
-                        <Button variant="outline" size="icon">
-                            <ShoppingCart />
-                        </Button>
-                    </CardFooter>
-                </Card>
-            ))}
+        <section className="p-4">
+            <Title title="My Products"/>
+            <div className="grid grid-cols-[repeat(auto-fill,minmax(min(200px,100%),1fr))] gap-4">
+                {storeData.products.map((product) => (
+                    <Card key={product.id} className="aspect-9/12 bg-accent hover:scale-105 transition-all cursor-pointer">
+                        <CardHeader>
+                            <CardTitle>{product.name}</CardTitle>
+                            <CardDescription className="line-clamp-2">{product.description || "No description available for this product"}</CardDescription>
+                        </CardHeader>
+                        <CardContent className="grow">
+                            <img src="https://api.dicebear.com/9.x/icons/svg?seed=boxes" alt="Product Image" className="rounded-md" />
+                        </CardContent>
+                        <CardFooter className="flex justify-between items-center">
+                            <p>${product.price}</p>
+                            <Button variant="outline" size="icon">
+                                <ShoppingCart />
+                            </Button>
+                        </CardFooter>
+                    </Card>
+                ))}
+            </div>
         </section>
     );
 }
