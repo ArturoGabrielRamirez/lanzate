7d288a612237f675362ef2962a61b2916ab6472c
Horacio Gutierrez <horacio.estevez@gmail.com>
2025-07-09 22:00:31 -0300

    Enhance StorePage layout with responsive card design
    - Replaced the previous product listing with a responsive grid layout using Tailwind CSS.
    - Integrated Card components for better visual presentation of products, including product name, description, and price.
    - Added a ShoppingCart button for each product, improving user interaction.
    - Ensured accessibility and responsiveness in the new design.

--- app/s/[subdomain]/page.tsx
+++ app/s/[subdomain]/page.tsx
@@ -1,4 +1,7 @@
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
 import { getStoreWithProducts } from "@/features/subdomain/actions/getStoreWithProducts";
+import { ShoppingCart } from "lucide-react";
 
 
 type Props = {
@@ -14,17 +17,24 @@ export default async function StorePage({ params }: Props) {
     }
 
     return (
-        <div>
-            <h1>Tienda: {storeData.store.name}</h1>
-            <div>
-                {storeData.products.map((product) => (
-                    <div key={product.id}>
-                        <h3>{product.name}</h3>
-                        <p>{product.description}</p>
-                        <p>Precio: ${product.price}</p>
-                    </div>
-                ))}
-            </div>
-        </div>
+        <section className="grid grid-cols-[repeat(auto-fill,minmax(min(200px,100%),1fr))] gap-4 p-4">
+            {storeData.products.map((product) => (
+                <Card key={product.id} className="aspect-9/12 bg-accent ">
+                    <CardHeader>
+                        <CardTitle>{product.name}</CardTitle>
+                        <CardDescription className="line-clamp-2">{product.description || "No description available for this product"}</CardDescription>
+                    </CardHeader>
+                    <CardContent className="grow">
+                        <img src="https://api.dicebear.com/9.x/icons/svg?seed=boxes" alt="Product Image" className="rounded-md" />
+                    </CardContent>
+                    <CardFooter className="flex justify-between items-center">
+                        <p>${product.price}</p>
+                        <Button variant="outline" size="icon">
+                            <ShoppingCart />
+                        </Button>
+                    </CardFooter>
+                </Card>
+            ))}
+        </section>
     );
 }
