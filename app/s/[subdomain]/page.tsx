7a6830e6e04c49307499ca00ead8a0da438c5b0d
Horacio Gutierrez <horacio.estevez@gmail.com>
2025-07-10 03:09:31 -0300

    Enhance StorePage with pagination and price range filtering
    - Introduced pagination controls to StorePage, allowing users to navigate through product listings more efficiently.
    - Added a price range filter component for improved product search capabilities.
    - Updated layout to maintain responsiveness and accessibility standards throughout the new design.

--- app/s/[subdomain]/page.tsx
+++ app/s/[subdomain]/page.tsx
@@ -5,7 +5,8 @@ import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@
 import { Title } from "@/features/layout/components";
 import CategorySelect from "@/features/store-landing/components/category-select-";
 import { getStoreWithProducts } from "@/features/subdomain/actions/getStoreWithProducts";
-import { Search, ShoppingCart } from "lucide-react";
+import { cn } from "@/lib/utils";
+import { ChevronLeft, ChevronRight, Grid, Grid2X2, List, Search, ShoppingCart } from "lucide-react";
 
 type Props = {
     params: Promise<{ subdomain: string }>
@@ -19,9 +20,14 @@ export default async function StorePage({ params }: Props) {
         return <div>Tienda no encontrada</div>;
     }
 
+    const paginationMaxAmount = 10;
+    const paginationCurrentPage = 1;
+    const paginationTotalPages = Math.ceil(storeData.products.length / paginationMaxAmount);
+
     return (
         <section className="p-4 grow flex flex-col">
             <Title title="My Products" />
+
             <div className="grid grid-cols-1 lg:grid-cols-[300px_1fr] xl:grid-cols-[350px_1fr] gap-4 grow">
                 <div className="flex flex-col gap-4">
                     <Card className="w-full">
@@ -69,25 +75,75 @@ export default async function StorePage({ params }: Props) {
                             </div>
                         </CardContent>
                     </Card>
+                    <Card className="w-full">
+                        <CardHeader>
+                            <CardTitle>Price Range</CardTitle>
+                        </CardHeader>
+                        <CardContent>
+                            <div className="flex gap-2">
+                                <Input placeholder="Min $" />
+                                <Input placeholder="Max $" />
+                            </div>
+                        </CardContent>
+                    </Card>
                 </div>
-                <div className="grid grid-cols-[repeat(auto-fill,minmax(min(200px,100%),1fr))] gap-4">
-                    {storeData.products.map((product) => (
-                        <Card key={product.id} className="aspect-9/12 bg-accent hover:scale-105 transition-all cursor-pointer">
-                            <CardHeader>
-                                <CardTitle>{product.name}</CardTitle>
-                                <CardDescription className="line-clamp-2">{product.description || "No description available for this product"}</CardDescription>
-                            </CardHeader>
-                            <CardContent className="grow">
-                                <img src="https://api.dicebear.com/9.x/icons/svg?seed=boxes" alt="Product Image" className="rounded-md" />
-                            </CardContent>
-                            <CardFooter className="flex justify-between items-center">
-                                <p>${product.price}</p>
-                                <Button variant="outline" size="icon">
-                                    <ShoppingCart />
+                <div className="flex flex-col gap-4 relative">
+                    <div className="flex gap-2 justify-between">
+                        <div className="flex gap-2">
+                            <Button variant="outline">
+                                <Grid2X2 />
+                            </Button>
+                            <Button variant="outline">
+                                <List />
+                            </Button>
+                        </div>
+                        <div className="flex gap-2 items-center">
+                            <Button variant="outline">
+                                <ChevronLeft />
+                            </Button>
+                            <p>{paginationCurrentPage} of {paginationTotalPages}</p>
+                            <Button variant="outline">
+                                <ChevronRight />
+                            </Button>
+                        </div>
+                    </div>
+                    <div className="flex gap-2">
+                        <p>Showing {paginationMaxAmount} of {storeData.products.length} products</p>
+                    </div>
+                    <div className="grid grid-cols-[repeat(auto-fill,minmax(min(200px,100%),1fr))] gap-4">
+                        {storeData.products.map((product) => (
+                            <Card key={product.id} className="aspect-9/12 bg-accent hover:scale-105 transition-all cursor-pointer">
+                                <CardHeader>
+                                    <CardTitle>{product.name}</CardTitle>
+                                    <CardDescription className="line-clamp-2">{product.description || "No description available for this product"}</CardDescription>
+                                </CardHeader>
+                                <CardContent className="grow">
+                                    <img src="https://api.dicebear.com/9.x/icons/svg?seed=boxes" alt="Product Image" className="rounded-md" />
+                                </CardContent>
+                                <CardFooter className="flex justify-between items-center">
+                                    <p>${product.price}</p>
+                                    <Button variant="outline" size="icon">
+                                        <ShoppingCart />
+                                    </Button>
+                                </CardFooter>
+                            </Card>
+                        ))}
+                    </div>
+                    <div className="flex justify-center gap-1 absolute bottom-4 left-0 right-0">
+                        <Button variant="outline" size="icon">
+                            <ChevronLeft />
+                        </Button>
+                        <div className="flex gap-2">
+                            {Array.from({ length: paginationTotalPages }).map((_, index) => (
+                                <Button key={index} variant="outline" size="icon" className={cn(paginationCurrentPage === index + 1 && "bg-primary text-primary-foreground")}>
+                                    {index + 1}
                                 </Button>
-                            </CardFooter>
-                        </Card>
-                    ))}
+                            ))}
+                        </div>
+                        <Button variant="outline" size="icon">
+                            <ChevronRight />
+                        </Button>
+                    </div>
                 </div>
             </div>
         </section>
