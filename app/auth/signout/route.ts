3d5f56da73e9a9e9a42925052c8073417e783b49
Horacio Gutierrez <horacio.estevez@gmail.com>
2025-07-11 02:23:54 -0300

    Update signout route and enhance header component with user authentication
    - Modified the signout route to redirect users to the login page after signing out.
    - Enhanced the Header component to display login and signup links when no user is authenticated, and an account dropdown when a user is logged in.
    - Ensured all changes adhere to responsiveness and accessibility standards.

--- app/auth/signout/route.ts
+++ app/auth/signout/route.ts
@@ -2,15 +2,19 @@ import { createServerSideClient } from '@/utils/supabase/server'
 import { revalidatePath } from 'next/cache'
 import { type NextRequest, NextResponse } from 'next/server'
 export async function POST(req: NextRequest) {
+
   const supabase = await createServerSideClient()
-  const {
-    data: { user },
-  } = await supabase.auth.getUser()
+
+  const { data: { user } } = await supabase.auth.getUser()
+
   if (user) {
     await supabase.auth.signOut()
   }
-  revalidatePath('/account', 'layout')
-  return NextResponse.redirect(new URL('/account', req.url), {
+
+  revalidatePath('/login', 'layout')
+
+  return NextResponse.redirect(new URL('/login', "http://localhost.com:3000"), {
     status: 302,
   })
+
 }
--- features/store-landing/components/header.tsx
+++ features/store-landing/components/header.tsx
@@ -1,12 +1,17 @@
 import { Rocket, ShoppingCart } from "lucide-react"
 import Link from "next/link"
 import { Button } from "@/components/ui/button"
+import { createServerSideClient } from "@/utils/supabase/server"
+import { AccountDropdown } from "@/features/header/components"
 
 type Props = {
     title: string
 }
 
-function Header({ title }: Props) {
+async function Header({ title }: Props) {
+
+    const supabase = await createServerSideClient()
+    const { data: { user } } = await supabase.auth.getUser()
 
     return (
         <header className="flex items-center justify-between w-full bg-accent text-accent-foreground p-4">
@@ -15,11 +20,14 @@ function Header({ title }: Props) {
                 <h1>{title}</h1>
             </Link>
             <div>
+                {!user && <Link href='/login' className='p-2 hover:underline hover:!text-primary'>Log In</Link>}
+                {!user && <Link href='/signup' className='p-2 hover:underline hover:text-primary'>Sign Up</Link>}
                 <Button variant="outline" asChild>
                     <Link href="/cart">
                         <ShoppingCart />
                     </Link>
                 </Button>
+                {user && <AccountDropdown />}
             </div>
         </header>
     )
