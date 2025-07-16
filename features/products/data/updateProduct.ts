9cb5d4b2c00c04502451acf29636dfd24ff7f9c6
Horacio Gutierrez <horacio.estevez@gmail.com>
2025-07-16 16:35:36 -0300

    Enhance EditProductButton with file upload functionality and image handling
    - Integrated a FileUpload component to allow users to upload product images in the EditProductButton.
    - Updated the handleEditProduct function to include image data in the product update payload.
    - Implemented file rejection handling with user feedback using toast notifications.
    - Modified the updateProduct function to manage image uploads to Supabase storage and update the product image URL in the database.
    - Ensured all changes adhere to TypeScript best practices and maintain responsiveness and accessibility standards.

--- features/products/data/updateProduct.ts
+++ features/products/data/updateProduct.ts
@@ -2,11 +2,13 @@
 
 import { PrismaClient } from "@/prisma/generated/prisma"
 import { formatErrorResponse } from "@/utils/lib"
+import { createServerSideClient } from "@/utils/supabase/server"
 
 export async function updateProduct(productId: number, data: any) {
     try {
 
         const prisma = new PrismaClient()
+        const supabase = createServerSideClient()
 
         const updatedProduct = await prisma.product.update({
             where: { id: productId },
@@ -21,6 +23,45 @@ export async function updateProduct(productId: number, data: any) {
 
             }
         })
+
+        if (data.image) {
+            if (data.image !== updatedProduct.image) {
+
+                const { data: payload, error } = await supabase.storage.from("product-images").upload(data.image.name, data.image)
+
+                console.log("🚀 ~ updateProduct ~ payload:", payload)
+                console.log("🚀 ~ updateProduct ~ error:", error)
+
+                if (error && error.statusCode != 409) throw new Error(error.message)
+
+                if (error && error.statusCode == 409) {
+
+                    const { data: { publicUrl } } = supabase.storage.from("product-images").getPublicUrl(data.image.name)
+
+                    console.log("🚀 ~ updateProduct ~ publicUrl:", publicUrl)
+
+                    await prisma.product.update({
+                        where: { id: updatedProduct.id },
+                        data: {
+                            image: publicUrl
+                        }
+                    })
+                }
+
+                if (payload) {
+
+                    const { data: { publicUrl } } = supabase.storage.from("product-images").getPublicUrl(payload.path)
+
+                    await prisma.product.update({
+                        where: { id: updatedProduct.id },
+                        data: {
+                            image: publicUrl
+                        }
+                    })
+                }
+
+            }
+        }
 
         return {
             message: "Product updated successfully",
