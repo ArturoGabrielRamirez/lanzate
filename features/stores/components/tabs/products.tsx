29753a5b90ae75c3ee96130228364c288ceb14ec
Horacio Gutierrez <horacio.estevez@gmail.com>
2025-07-18 17:29:07 -0300

    Refactor ProductsTab component for improved product display
    - Removed commented-out Card component and ProductCard mapping for a cleaner codebase.
    - Introduced a flex container for the CreateProductButton to enhance layout and accessibility.
    - Ensured all changes adhere to TypeScript best practices and maintain responsiveness and accessibility standards.

--- features/stores/components/tabs/products.tsx
+++ features/stores/components/tabs/products.tsx
@@ -1,11 +1,5 @@
 import { getStoresFromSlug } from "../../actions/getStoresFromSlug"
-
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
 import { CreateProductButton } from "@/features/products/components"
-import { ProductCard } from "@/features/stores/components"
-
-import { Plus } from "lucide-react"
-
 import { ProductsTabProps } from "@/features/stores/types"
 import ProductsTable from "../products-table"
 
@@ -18,21 +12,10 @@ async function ProductsTab({ slug }: ProductsTabProps) {
     }
 
     return (
-        <div /* className="grid grid-cols-1 lg:grid-cols-[repeat(auto-fill,minmax(min(200px,100%),1fr))] gap-4 w-full" */>
-            {/* <Card className="border-dashed">
-                <CardHeader>
-                    <CardTitle className="flex items-center gap-2">
-                        <Plus className="size-4" />
-                        <h3 className="font-bold text-sm">New Product</h3>
-                    </CardTitle>
-                </CardHeader>
-                <CardContent className="flex justify-center items-center grow">
-                    <CreateProductButton storeId={store.id} />
-                </CardContent>
-            </Card> */}
-            {/* {store.products.map((product) => (
-                <ProductCard key={product.id} product={product} slug={slug} />
-            ))} */}
+        <div>
+            <div className="flex justify-end mb-4">
+                <CreateProductButton storeId={store.id} />
+            </div>
             <ProductsTable data={store.products} />
         </div>
     )
