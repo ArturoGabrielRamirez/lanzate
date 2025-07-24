3e9604b0fc7b4330397163dbe88e240d1e2d2ff3
ArturoGabrielRamirez <jiamafox@gmail.com>
2025-07-24 04:31:47 -0300

    feat: Remove obsolete authentication and store management routes and components

--- app/check-email/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function CheckEmail() {
-    return (
-        <div className="p-8 text-center grow --color-background text-white flex flex-col items-center justify-center">
-            <h2 className="text-2xl font-bold mb-4">Please check your email for the confirmation link.</h2>
-            <p>We have sent you a confirmation link. Please verify it to complete your registration.</p>
-        </div>
-    )
-}
--- app/stores/[slug]/page.tsx
+++
@@ -1,9 +0,0 @@
-async function StoreDetailsPage() {
-
-    return (
-        <div className="p-4 grow flex flex-col">
-            Store details page
-        </div>
-    )
-}
-export default StoreDetailsPage
