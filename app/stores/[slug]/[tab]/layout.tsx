8570fbd6565f44e81800898b8a6cf1e0f26da855
Horacio Gutierrez <horacio.estevez@gmail.com>
2025-07-24 03:29:15 -0300

    Enhance Tab Layout with Icon Integration
    - Updated the TabLayout component to include icons for each tab title, improving visual clarity and user experience.
    - Utilized Lucide icons to represent different sections such as Overview, Account Details, and more.
    - Ensured adherence to TypeScript best practices and maintained responsiveness and accessibility standards.

--- app/stores/[slug]/[tab]/layout.tsx
+++ app/stores/[slug]/[tab]/layout.tsx
@@ -2,28 +2,29 @@ import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
 import { TabsContent } from "@/components/ui/tabs"
 
 import { TabLayoutProps } from "@/features/stores/types"
+import { BookOpenText, Building2, UsersRound, Box, Store, ShoppingCart, Settings, ChartLine, Clock } from "lucide-react"
 
 async function TabLayout({ children, params }: TabLayoutProps) {
 
     const { tab } = await params
 
     const titles = {
-        overview: "Overview",
-        account: "Account Details",
-        branches: "Branches",
-        employees: "Employees",
-        products: "Products",
-        orders: "Orders",
-        settings: "Settings",
-        analytics: "Analytics",
-        history: "History",
+        overview: <><BookOpenText className="w-4 h-4" />Overview</>,
+        account: <><Store className="w-4 h-4" />Account Details</>,
+        branches: <><Building2 className="w-4 h-4" />Branches</>,
+        employees: <><UsersRound className="w-4 h-4" />Employees</>,
+        products: <><Box className="w-4 h-4" />Products</>,
+        orders: <><ShoppingCart className="w-4 h-4" />Orders</>,
+        settings: <><Settings className="w-4 h-4" />Settings</>,
+        analytics: <><ChartLine className="w-4 h-4" />Analytics</>,
+        history: <><Clock className="w-4 h-4" />History</>,
     }
 
     return (
         <TabsContent value={tab} className="flex flex-col">
             <Card className="grow">
                 <CardHeader>
-                    <CardTitle>{titles[tab as keyof typeof titles]}</CardTitle>
+                    <CardTitle className="flex items-center gap-2">{titles[tab as keyof typeof titles]}</CardTitle>
                 </CardHeader>
                 <CardContent className="gap-4 flex flex-col grow">
                     {children}
