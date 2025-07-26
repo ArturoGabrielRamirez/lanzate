d44776797a82df9fd0bcb78b41dd434b608a9e9c
Horacio Gutierrez <horacio.estevez@gmail.com>
2025-07-26 20:29:05 -0300

    feat: Enhance FeaturesSection with animations for improved user engagement
    - Integrated motion effects using motion.div for dynamic animations in the FeaturesSection.
    - Added animation variants for smooth transitions when elements come into view.
    - Ensured all changes adhere to TypeScript best practices and maintain responsive design principles using Tailwind CSS.

--- features/landing/components/features-section.tsx
+++ features/landing/components/features-section.tsx
@@ -1,34 +1,67 @@
+import * as motion from "motion/react-client"
 import { useTranslations } from "next-intl";
 
 function FeaturesSection() {
 
     const t = useTranslations('home');
 
+    const variants = {
+        hidden: { opacity: 0, y: 100, scale: 0.9 },
+        visible: {
+            opacity: 1,
+            y: 0,
+            scale: 1,
+            transition: {
+                staggerChildren: 0.15,
+            }
+        },
+    }
+
+    const itemVariants = {
+        hidden: { opacity: 0, y: 100, scale: 0.9 },
+        visible: {
+            opacity: 1,
+            y: 0,
+            scale: 1,
+        },
+    }
+
     return (
         <section className="p-6">
             <div className="container mx-auto">
                 <span className="block mb-2 text-xs font-medium tracking-widest text-center uppercase text-accent">{t('description.how-it-work.title')}</span>
                 <h2 className="text-5xl font-bold text-center">{t('description.how-it-work.description')}</h2>
-                <div className="grid gap-6 my-16 lg:grid-cols-3">
-                    <div className="flex flex-col p-8 space-y-4 rounded-md bg-primary text-primary-foreground">
+                <motion.div className="grid gap-6 my-16 lg:grid-cols-3"
+                    initial="hidden"
+                    whileInView="visible"
+                    viewport={{ once: false, amount: 0.15 }}
+                    variants={variants}
+                >
+                    <motion.div className="flex flex-col p-8 space-y-4 rounded-md bg-primary text-primary-foreground"
+                        variants={itemVariants}
+                    >
                         <div className="flex items-center justify-center flex-shrink-0 w-12 h-12 text-xl font-bold rounded-full bg-accent ">1</div>
                         <p className="text-2xl font-semibold">
                             <b>{t('description.centralize.title')}</b> {t('description.centralize.description')}
                         </p>
-                    </div>
-                    <div className="flex flex-col p-8 space-y-4 rounded-md bg-primary text-primary-foreground">
+                    </motion.div>
+                    <motion.div className="flex flex-col p-8 space-y-4 rounded-md bg-primary text-primary-foreground"
+                        variants={itemVariants}
+                    >
                         <div className="flex items-center justify-center flex-shrink-0 w-12 h-12 text-xl font-bold rounded-full bg-accent ">2</div>
                         <p className="text-2xl font-semibold">
                             <b>{t('description.sell.title')} &amp; {t('description.sell.title-2')}</b> {t('description.sell.description')}
                         </p>
-                    </div>
-                    <div className="flex flex-col p-8 space-y-4 rounded-md bg-primary text-primary-foreground">
+                    </motion.div>
+                    <motion.div className="flex flex-col p-8 space-y-4 rounded-md bg-primary text-primary-foreground"
+                        variants={itemVariants}
+                    >
                         <div className="flex items-center justify-center flex-shrink-0 w-12 h-12 text-xl font-bold rounded-full bg-accent ">3</div>
                         <p className="text-2xl font-semibold">
                             <b>{t('description.ship-easily.title')}</b> {t('description.ship-easily.description')}
                         </p>
-                    </div>
-                </div>
+                    </motion.div>
+                </motion.div>
             </div>
         </section>
     )
