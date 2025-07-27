1282da11770e8ac63862a8694e680cc5fe9f2db6
Horacio Gutierrez <horacio.estevez@gmail.com>
2025-07-26 21:34:40 -0300

    feat: Integrate Embla Carousel for enhanced visual presentation in FeaturesSection
    - Added Embla Carousel components to FeaturesSection for improved content display and user engagement.
    - Implemented autoplay functionality for seamless transitions between features.
    - Updated feature descriptions and images to align with new carousel layout.
    - Ensured adherence to TypeScript best practices and maintained responsive design principles using Tailwind CSS.

--- features/landing/components/features-section.tsx
+++ features/landing/components/features-section.tsx
@@ -1,5 +1,9 @@
+"use client"
 import * as motion from "motion/react-client"
 import { useTranslations } from "next-intl";
+import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from "@/components/ui/carousel"
+import Autoplay from "embla-carousel-autoplay"
+import Image from "next/image";
 
 function FeaturesSection() {
 
@@ -30,41 +34,60 @@ function FeaturesSection() {
                 <motion.h2
                     initial={{ opacity: 0, y: 100 }}
                     whileInView={{ opacity: 1, y: 0, transition: { delay: 0.2 } }}
-                    className="text-5xl font-bold text-center"
+                    className="text-5xl font-bold text-center mb-22"
                 >
                     {t('description.how-it-work.description')}
                 </motion.h2>
-                <motion.div className="grid gap-6 my-16 lg:grid-cols-3"
-                    initial="hidden"
-                    whileInView="visible"
-                    viewport={{ once: false, amount: 0.15 }}
-                    variants={variants}
+                <Carousel
+                    plugins={[
+                        Autoplay({ delay: 2000 })
+                    ]}
+                    opts={{ loop: true }}
+                    className="container mx-auto mask-l-from-60% mask-l-to-90% mask-r-from-60% mask-r-to-90%"
                 >
-                    <motion.div className="flex flex-col p-8 space-y-4 rounded-md bg-primary text-primary-foreground"
-                        variants={itemVariants}
-                    >
-                        <div className="flex items-center justify-center flex-shrink-0 w-12 h-12 text-xl font-bold rounded-full bg-accent ">1</div>
-                        <p className="text-2xl font-semibold">
-                            <b>{t('description.centralize.title')}</b> {t('description.centralize.description')}
-                        </p>
-                    </motion.div>
-                    <motion.div className="flex flex-col p-8 space-y-4 rounded-md bg-primary text-primary-foreground"
-                        variants={itemVariants}
-                    >
-                        <div className="flex items-center justify-center flex-shrink-0 w-12 h-12 text-xl font-bold rounded-full bg-accent ">2</div>
-                        <p className="text-2xl font-semibold">
-                            <b>{t('description.sell.title')} &amp; {t('description.sell.title-2')}</b> {t('description.sell.description')}
-                        </p>
-                    </motion.div>
-                    <motion.div className="flex flex-col p-8 space-y-4 rounded-md bg-primary text-primary-foreground"
-                        variants={itemVariants}
-                    >
-                        <div className="flex items-center justify-center flex-shrink-0 w-12 h-12 text-xl font-bold rounded-full bg-accent ">3</div>
-                        <p className="text-2xl font-semibold">
-                            <b>{t('description.ship-easily.title')}</b> {t('description.ship-easily.description')}
-                        </p>
-                    </motion.div>
-                </motion.div>
+                    <CarouselContent className="">
+                        <CarouselItem className="basis-1/2 grow grid grid-cols-2 gap-1">
+                            <motion.div className="flex flex-col p-8 h-full rounded-md bg-primary text-primary-foreground gap-22">
+                                <motion.div className="flex items-center justify-center flex-shrink-0 w-12 h-12 text-xl font-bold rounded-full bg-accent">
+                                    1
+                                </motion.div>
+                                <p className="text-2xl font-semibold">
+                                    <b>{t('description.centralize.title')}</b> {t('description.centralize.description')}
+                                </p>
+                            </motion.div>
+                            <div className="relative rounded-md overflow-hidden">
+                                <Image
+                                    src="/landing/feature-1.jpg"
+                                    alt="Centralize"
+                                    fill
+                                    className="object-cover"
+                                />
+                            </div>
+                        </CarouselItem>
+                        <CarouselItem className="basis-1/2 grow">
+                            <motion.div className="flex flex-col p-8 h-full rounded-md bg-primary text-primary-foreground"
+                                variants={itemVariants}
+                            >
+                                <div className="flex items-center justify-center flex-shrink-0 w-12 h-12 text-xl font-bold rounded-full bg-accent ">2</div>
+                                <p className="text-2xl font-semibold">
+                                    <b>{t('description.sell.title')} &amp; {t('description.sell.title-2')}</b> {t('description.sell.description')}
+                                </p>
+                            </motion.div>
+                        </CarouselItem>
+                        <CarouselItem className="basis-1/2 grow">
+                            <motion.div className="flex flex-col p-8 h-full rounded-md bg-primary text-primary-foreground"
+                                variants={itemVariants}
+                            >
+                                <div className="flex items-center justify-center flex-shrink-0 w-12 h-12 text-xl font-bold rounded-full bg-accent ">3</div>
+                                <p className="text-2xl font-semibold">
+                                    <b>{t('description.ship-easily.title')}</b> {t('description.ship-easily.description')}
+                                </p>
+                            </motion.div>
+                        </CarouselItem>
+                    </CarouselContent>
+                    <CarouselPrevious />
+                    <CarouselNext />
+                </Carousel>
             </div>
         </section>
     )
