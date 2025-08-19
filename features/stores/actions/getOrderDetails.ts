bcb92e692b26f02b20f9a7a8f0f050792d62dccf
ArturoGabrielRamirez <jiamafox@gmail.com>
2025-08-19 16:00:35 -0300

    add store: Store at getOrderDetails

--- features/stores/actions/getOrderDetails.ts
+++ features/stores/actions/getOrderDetails.ts
@@ -2,10 +2,12 @@
 
 import { actionWrapper } from "@/utils/lib"
 import { selectOrderById } from "../data/selectOrderById"
-import { Order, OrderItem, OrderPayment, OrderTracking, Product } from "@prisma/client"
+import { Order, OrderItem, OrderPayment, OrderTracking, Product, Store } from "@prisma/client"
 
 type GetOrderDetailsResponse = {
-    payload: Order & { tracking: OrderTracking | null, items: (OrderItem & { product: Product })[] } & { payment: OrderPayment } | null
+    payload: Order & { tracking: OrderTracking | null, items: (OrderItem & { product: Product })[] } & { payment: OrderPayment } & {
+        store: Store
+    } | null
     error: boolean
     message: string
 }
