988ab48433c5ce150b999e0d0450c855ef7ad283
ArturoGabrielRamirez <jiamafox@gmail.com>
2025-08-19 16:47:07 -0300

    add details to dont forget that error

--- features/stores/components/store-card.tsx
+++ features/stores/components/store-card.tsx
@@ -26,14 +26,17 @@ async function StoreCard({ store }: StoreCardProps) {
                         <Calendar className="size-4" />
                         {store.created_at.toLocaleDateString()}
                     </p>
-                    <p className="text-xl text-red-500">
-                        ALTO AHÍ HUMAN O! ESTA SECCIÓN REQUIERE DE SU INCOMPETENCIA.
-                        <span>
-                            store._count?.products, no existe.
-                        </span>
-                       {/*  {store._count?.products || 0} {t("products")} */}
-                    </p>
                 </CardFooter>
+                <h3 className="text-md text-red-500 pl-2">
+                    ALTO AHÍ HUMAN O!
+                    {/*  {store._count?.products || 0} {t("products")} */}
+                </h3>
+                <p className="text-sm text-red-500 pl-2">
+                    ESTA SECCIÓN REQUIERE DE SU INCOMPETENCIA.
+                </p>
+                <span className="pl-2">
+                    store._count?.products, no existe.
+                </span>
             </Card>
         </Link>
     )
