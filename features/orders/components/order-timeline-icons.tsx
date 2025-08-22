f6b943c9b4bc28330d65f517dfcc08f7b1139bd6
Horacio Gutierrez <horacio.estevez@gmail.com>
2025-08-21 23:50:54 -0300

    Add order timeline icons component enhancements and log file
    - Introduce a new log file for order access tracking.
    - Refactor OrderTimelineIcons component to improve conditional rendering of skeletons based on order tracking status.
    - Streamline tooltip structure for better readability and maintainability.
    - Ensure consistent styling and responsiveness using Tailwind CSS.

--- features/orders/components/order-timeline-icons.tsx
+++ features/orders/components/order-timeline-icons.tsx
@@ -12,7 +12,7 @@ type Props = {
 const OrderTimelineIcons = ({ order }: Props) => {
     const isProcessing = order.status === "PROCESSING"
     const isReady = order.status === "READY"
-    
+
     const isPreparingOrder = isReady && order.tracking?.tracking_status === "PREPARING_ORDER"
     const isWaitingForDelivery = isReady && order.tracking?.tracking_status === "WAITING_FOR_DELIVERY"
     const isOnTheWay = order.tracking?.tracking_status === "ON_THE_WAY"
@@ -43,7 +43,7 @@ const OrderTimelineIcons = ({ order }: Props) => {
                     <p>The order was just confirmed and is waiting for the store to start packing it</p>
                 </TooltipContent>
             </Tooltip>
-            <Skeleton className={cn("w-5 h-1", isReady && "bg-green-600")} />
+            <Skeleton className={cn("w-5 h-1", isReady && order.tracking?.tracking_status === "PREPARING_ORDER" && "bg-green-600")} />
             <Tooltip>
                 <TooltipTrigger>
                     <IconButton
@@ -59,34 +59,35 @@ const OrderTimelineIcons = ({ order }: Props) => {
             <Skeleton className={cn("w-5 h-1", isPreparingOrder && "bg-green-600")} />
 
             {order.shipping_method === "DELIVERY" && (
-                <Tooltip>
-                    <TooltipTrigger>
-                        <IconButton
-                            size={order.status === "READY" && order.tracking?.tracking_status === "WAITING_FOR_DELIVERY" ? "lg" : "md"}
-                            className={cn(
-                                "text-muted-foreground/50",
-                                order.tracking?.tracking_status === "WAITING_FOR_DELIVERY" && "text-green-600 animate-pulse"
-                            )}
-                            icon={
-                                () => (
-                                    <svg
-                                        xmlns="http://www.w3.org/2000/svg"
-                                        className="size-5"
-                                        width={32}
-                                        height={32}
-                                        viewBox="0 0 24 24">
-                                        <g fill="none" stroke="currentColor" strokeWidth={1.5}><path d="M19.5 19.5a2.5 2.5 0 1 1-5 0a2.5 2.5 0 0 1 5 0Zm-10 0a2.5 2.5 0 1 1-5 0a2.5 2.5 0 0 1 5 0Z"></path><path strokeLinecap="round" strokeLinejoin="round" d="M14.5 19.5h-5m10 0h.763c.22 0 .33 0 .422-.012a1.5 1.5 0 0 0 1.303-1.302c.012-.093.012-.203.012-.423V15a6.5 6.5 0 0 0-6.5-6.5M11 6h1c1.414 0 2.121 0 2.56.44C15 6.878 15 7.585 15 9v8.5M2 12v5c0 .935 0 1.402.201 1.75a1.5 1.5 0 0 0 .549.549c.348.201.815.201 1.75.201M7.85 7.85l-1.35-.9V4.7M2 6.5a4.5 4.5 0 1 0 9 0a4.5 4.5 0 0 0-9 0"></path></g>
-                                    </svg>
+                <>
+                    <Tooltip>
+                        <TooltipTrigger>
+                            <IconButton
+                                size={order.status === "READY" && order.tracking?.tracking_status === "WAITING_FOR_DELIVERY" ? "lg" : "md"}
+                                className={cn(
+                                    "text-muted-foreground/50",
+                                    order.tracking?.tracking_status === "WAITING_FOR_DELIVERY" && "text-green-600 animate-pulse"
                                 )}
-                        />
-                    </TooltipTrigger>
-                    <TooltipContent>
-                        <p>The order is ready for delivery and is waiting for the delivery driver to pick it up</p>
-                    </TooltipContent>
-                </Tooltip>
-
+                                icon={
+                                    () => (
+                                        <svg
+                                            xmlns="http://www.w3.org/2000/svg"
+                                            className="size-5"
+                                            width={32}
+                                            height={32}
+                                            viewBox="0 0 24 24">
+                                            <g fill="none" stroke="currentColor" strokeWidth={1.5}><path d="M19.5 19.5a2.5 2.5 0 1 1-5 0a2.5 2.5 0 0 1 5 0Zm-10 0a2.5 2.5 0 1 1-5 0a2.5 2.5 0 0 1 5 0Z"></path><path strokeLinecap="round" strokeLinejoin="round" d="M14.5 19.5h-5m10 0h.763c.22 0 .33 0 .422-.012a1.5 1.5 0 0 0 1.303-1.302c.012-.093.012-.203.012-.423V15a6.5 6.5 0 0 0-6.5-6.5M11 6h1c1.414 0 2.121 0 2.56.44C15 6.878 15 7.585 15 9v8.5M2 12v5c0 .935 0 1.402.201 1.75a1.5 1.5 0 0 0 .549.549c.348.201.815.201 1.75.201M7.85 7.85l-1.35-.9V4.7M2 6.5a4.5 4.5 0 1 0 9 0a4.5 4.5 0 0 0-9 0"></path></g>
+                                        </svg>
+                                    )}
+                            />
+                        </TooltipTrigger>
+                        <TooltipContent>
+                            <p>The order is ready for delivery and is waiting for the delivery driver to pick it up</p>
+                        </TooltipContent>
+                    </Tooltip>
+                    <Skeleton className={cn("w-5 h-1", isWaitingForDelivery && "bg-green-600")} />
+                </>
             )}
-            <Skeleton className={cn("w-5 h-1", isWaitingForDelivery && "bg-green-600")} />
             {order.shipping_method === "DELIVERY" && (
                 <>
                     <Tooltip>
