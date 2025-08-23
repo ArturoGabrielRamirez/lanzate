9082766080fb1c363898c3151a1806438a0b98bd
Horacio Gutierrez <horacio.estevez@gmail.com>
2025-08-22 22:07:38 -0300

    Enhance StorePage layout with new category cards and banner image
    - Introduce a new banner image for visual appeal and improved user experience.
    - Add category cards for Candy, Food, Drinks, Library, and View All, enhancing navigation and product discovery.
    - Comment out unused imports and code for cleaner structure and maintainability.
    - Ensure responsive design with Tailwind CSS for better user interaction.

--- app/[locale]/s/[subdomain]/page.tsx
+++ app/[locale]/s/[subdomain]/page.tsx
@@ -1,16 +1,17 @@
-import { Title } from "@/features/layout/components";
+/* import { Title } from "@/features/layout/components"; */
+import { Card, CardContent } from "@/components/ui/card";
 import PageContainer from "@/features/layout/components/page-container";
 import PaginationNav from "@/features/store-landing/components/pagination-nav";
 import ProductAmountDisplay from "@/features/store-landing/components/product-amount-display";
 import ProductCardLoader from "@/features/store-landing/components/product-card-loader";
 import ProductList from "@/features/store-landing/components/product-list";
 import ProductListDisplay from "@/features/store-landing/components/product-list-display";
-import SectionContainer from "@/features/store-landing/components/section-container";
-import SidebarFilters from "@/features/store-landing/components/sidebar-filters";
+/* import SectionContainer from "@/features/store-landing/components/section-container";
+import SidebarFilters from "@/features/store-landing/components/sidebar-filters"; */
 import { loadFilterParams } from "@/features/store-landing/utils/load-filter-params";
 import { getStoreProductAmount } from "@/features/subdomain/actions/getStoreProductAmount";
-import { BadgeAlert, Clock, Crown, Home, Trophy } from "lucide-react";
-import { getTranslations } from "next-intl/server";
+import { ArrowRight, BadgeAlert, BookA, Candy, Clock, Crown, Eye, Home, Milk, Trophy, Utensils } from "lucide-react";
+import Image from "next/image";
 import Link from "next/link";
 import { SearchParams } from "nuqs";
 import { Suspense } from "react";
@@ -25,7 +26,7 @@ export default async function StorePage({ params, searchParams }: Props) {
     const { category, sort, search, min, max, page, limit } = await loadFilterParams(searchParams)
 
     const { payload: productAmount } = await getStoreProductAmount(subdomain);
-    const t = await getTranslations("subdomain");
+    /* const t = await getTranslations("subdomain"); */
 
     return (
         <>
@@ -56,6 +57,68 @@ export default async function StorePage({ params, searchParams }: Props) {
                             <Clock className="size-4" />
                             Coming Soon
                         </Link>
+                    </div>
+                </div>
+                <div className="relative h-40 w-full brightness-50 hover:brightness-100 transition-all duration-300 group overflow-hidden">
+                    <Image
+                        src="/public-store/banner.jpg"
+                        alt="Banner"
+                        fill
+                        objectFit="cover"
+                        className="group-hover:scale-105 transition-all duration-300"
+                    />
+                </div>
+                <div className="container mx-auto py-8 group">
+                    <div className="flex justify-between items-center mb-8">
+                        <h2 className="text-2xl font-bold text-primary-foreground/50 group-hover:text-primary-foreground transition-all duration-300">
+                            Browse by Category
+                        </h2>
+                        <Link href="#" className="flex items-center gap-2 text-sm text-primary-foreground/50 hover:text-primary transition-all duration-300">
+                            <ArrowRight className="size-4" />
+                            View All
+                        </Link>
+                    </div>
+                    <div className="flex gap-4">
+                        <Card className="w-full rounded-none bg-primary/50 hover:bg-primary transition-all duration-300 hover:rounded-lg hover:scale-105">
+                            <CardContent className="grow flex justify-center flex-col items-center">
+                                <Candy className="size-10" />
+                                <p>
+                                    Candy
+                                </p>
+                            </CardContent>
+                        </Card>
+                        <Card className="w-full rounded-none bg-primary/50 hover:bg-primary transition-all duration-300 hover:rounded-lg hover:scale-105">
+                            <CardContent className="grow flex justify-center flex-col items-center">
+                                <Utensils className="size-10" />
+                                <p>
+                                    Food
+                                </p>
+                            </CardContent>
+                        </Card>
+                        <Card className="w-full rounded-none bg-primary/50 hover:bg-primary transition-all duration-300 hover:rounded-lg hover:scale-105">
+                            <CardContent className="grow flex justify-center flex-col items-center">
+                                <Milk className="size-10" />
+                                <p>
+                                    Drinks
+                                </p>
+                            </CardContent>
+                        </Card>
+                        <Card className="w-full rounded-none bg-primary/50 hover:bg-primary transition-all duration-300 hover:rounded-lg hover:scale-105">
+                            <CardContent className="grow flex justify-center flex-col items-center">
+                                <BookA className="size-10" />
+                                <p>
+                                    Library
+                                </p>
+                            </CardContent>
+                        </Card>
+                        <Card className="w-full rounded-none bg-primary/50 hover:bg-primary transition-all duration-300 hover:rounded-lg hover:scale-105">
+                            <CardContent className="grow flex justify-center flex-col items-center">
+                                <Eye className="size-10" />
+                                <p>
+                                    View All
+                                </p>
+                            </CardContent>
+                        </Card>
                     </div>
                 </div>
                 {/* <Title title={t("title")} /> */}
