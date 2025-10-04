2aa3f6959a1bb9bc743c38cfcd5b42db1738c268
Horacio Gutierrez <horacio.estevez@gmail.com>
2025-10-04 02:55:23 -0300

    refactor VariantsStep to manage product variants based on selected attributes, enhancing state management and user experience. Implement logic for sizes, dimensions, colors, materials, flavors, and fragrances, ensuring consistent data structure across components.

--- features/products/components/new-product-dialog/variants-step.tsx
+++ features/products/components/new-product-dialog/variants-step.tsx
@@ -20,8 +20,36 @@ const VariantsStep = () => {
         const variants = []
 
         if (selected_attributes.includes("Talle")) {
-            const sizes = form.getValues("sizes") || [] //["XS","S"]
-            console.log(sizes)
+            const sizes = form.getValues("sizes") || [] //[{id: 1, label: "XS"}, {id: 2, label: "S"}]
+            for (const size of sizes) {
+                variants.push({
+                    size : size.label,
+                    size_id: size.id,
+                })
+            }
+        }
+
+        if (selected_attributes.includes("Dimension")) {
+            const dimensions = form.getValues("dimensions") || [] //[{id: 1, label: "100x100"}, {id: 2, label: "200x200"}]
+            for (const dimension of dimensions) {
+                
+            }
+        }
+
+        if (selected_attributes.includes("Color")) {
+            const colors = form.getValues("colors") || [] //[{id: 1, label: "Red"}, {id: 2, label: "Blue"}]
+        }
+
+        if (selected_attributes.includes("Material")) {
+            const materials = form.getValues("materials") || [] //[{id: 1, label: "Wood"}, {id: 2, label: "Metal"}]
+        }
+
+        if (selected_attributes.includes("Sabor")) {
+            const flavors = form.getValues("flavors") || [] //[{id: 1, label: "Vanilla"}, {id: 2, label: "Chocolate"}]
+        }
+
+        if (selected_attributes.includes("Fragancia")) {
+            const fragrances = form.getValues("fragrances") || [] //[{id: 1, label: "Vanilla"}, {id: 2, label: "Chocolate"}]
         }
 
         return variants
