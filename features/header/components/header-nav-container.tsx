9702ab6a4ae668271f6ea995e90984b7c498f71e
Horacio Gutierrez <horacio.estevez@gmail.com>
2025-10-11 18:36:34 -0300

    Update HeaderNavContainer to enhance background blur effect and styling
    - Adjusted the scroll threshold for background blur from window height to a fixed value of 100 for improved responsiveness.
    - Enhanced the navigation styling with additional border and shadow effects for a more polished appearance.

--- features/header/components/header-nav-container.tsx
+++ features/header/components/header-nav-container.tsx
@@ -14,7 +14,7 @@ function HeaderNavContainer({ children }: { children: ReactNode }) {
 
     useMotionValueEvent(scrollY, "change", (latest) => {
         const windowHeight = window.innerHeight
-        if (latest > windowHeight) {
+        if (latest > 100) {
             setShouldBlurBackground(true)
         } else {
             setShouldBlurBackground(false)
@@ -28,7 +28,7 @@ function HeaderNavContainer({ children }: { children: ReactNode }) {
 
 
     return (
-        <nav className={cn("hidden xl:flex items-center fixed left-1/2 -translate-x-1/2 gap-8 transition-all", shouldBlurBackground && "backdrop-blur-sm bg-primary/50 rounded-full py-3 mt-4 px-6")}>
+        <nav className={cn("hidden xl:flex items-center fixed left-1/2 -translate-x-1/2 gap-8 transition-all", shouldBlurBackground && "backdrop-blur-sm bg-primary/50 border-t border-t-[color-mix(in_oklch,var(--primary),white_30%)] border-b border-b-[color-mix(in_oklch,var(--primary)_50%,black_5%)] rounded-full py-3 mt-4 px-6 shadow-2xl drop-shadow-2xl")}>
             {shouldBlurBackground && (
                 <motion.div {...headerLogoConfig}>
                     <HeaderLogo hasText={false} />
