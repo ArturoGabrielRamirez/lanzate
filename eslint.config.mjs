164e54a5c7cad6fc57fb168a25440931b77ca41a
Horacio Gutierrez <horacio.estevez@gmail.com>
2025-10-12 03:35:28 -0300

    delete multiple access log files and update ESLint configuration to include React and import plugins with enhanced rules for better code organization and clarity

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,9 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
+
 import { FlatCompat } from "@eslint/eslintrc";
+import pluginImport from "eslint-plugin-import";
+import reactPlugin from "eslint-plugin-react";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -10,7 +13,74 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends(
+    "next/core-web-vitals",
+    "next/typescript",
+    "plugin:react/recommended",
+  ),
+  {
+    plugins: {
+      import: pluginImport,
+      react: reactPlugin,
+    },
+    languageOptions: {
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+    rules: {
+      "import/order": [
+        "error",
+        {
+          groups: [
+            "builtin", // Node built-ins
+            "external", // npm packages
+            "internal", // aliases como @/*
+            ["parent", "sibling", "index"],
+            "object",
+            "type",
+          ],
+          pathGroups: [
+            {
+              pattern: "@/**",
+              group: "internal",
+              position: "before",
+            },
+            {
+              pattern: "./*.css",
+              group: "index",
+              position: "after",
+            },
+          ],
+          pathGroupsExcludedImportTypes: ["builtin"],
+          "newlines-between": "always",
+          alphabetize: {
+            order: "asc",
+            caseInsensitive: true,
+          },
+          warnOnUnassignedImports: true,
+        }
+      ],
+      "no-restricted-imports": [
+        "error",
+        {
+          patterns: [
+            {
+              group: ["../*", "./*"],
+              message:
+                "Usá el alias '@/...' en lugar de imports relativos. Esto mantiene el orden y la claridad del código.",
+            },
+          ],
+        },
+      ],
+      "react/react-in-jsx-scope": "off",
+      "react/function-component-definition": [
+        "error"
+      ],
+    }
+  }
 ];
 
 export default eslintConfig;
