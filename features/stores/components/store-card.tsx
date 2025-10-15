c724cbedc114409634e6d70b723a909e37668cce
Horacio Gutierrez <horacio.estevez@gmail.com>
2025-10-15 20:15:08 -0300

    Refactor StoreCard component for improved structure and functionality
    - Converted StoreCard to a client component and updated translation handling to use useTranslations.
    - Enhanced the layout by restructuring the Card component, including a new DropDrawer for additional actions.
    - Improved accessibility by wrapping the store name in a Link for better navigation.
    - Cleaned up imports and organized component structure for maintainability.

--- features/stores/components/store-card.tsx
+++ features/stores/components/store-card.tsx
@@ -1,46 +1,66 @@
-import { Calendar, StoreIcon } from "lucide-react"
+"use client"
+import { Calendar, EllipsisVertical, StoreIcon } from "lucide-react"
 import Image from "next/image"
 import Link from "next/link"
-import { getTranslations } from "next-intl/server"
+import { useTranslations } from "next-intl"
 
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
-import { Card, CardContent, CardFooter, CardHeader, CardTitle } from "@/components/ui/card"
+import { Card, CardAction, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card"
+import { DropDrawer, DropDrawerContent, DropDrawerTrigger } from "@/features/shadcn/components/components/ui/dropdrawer"
 import { StoreCardProps } from "@/features/stores/types"
+import { IconButton } from "@/src/components/ui/shadcn-io/icon-button"
 
-async function StoreCard({ store }: StoreCardProps) {
+function StoreCard({ store }: StoreCardProps) {
 
-    const t = await getTranslations("store.store-card")
+    const t = useTranslations("store.store-card")
 
     return (
-        <Link href={`/stores/${store.slug}/account`}>
-            <Card className="cursor-pointer transition-all gap-2 md:gap-3 lg:gap-4 shrink-0 group h-full group relative">
-                <CardHeader className="gap-0">
-                    <CardTitle className="flex items-center gap-2">
-                        <Avatar className="aspect-square size-8 lg:size-10">
-                            <AvatarImage src={store.logo || ""} alt={store.name} asChild className="aspect-square">
-                                <Image src={store.logo || ""} alt={store.name} width={32} height={32} unoptimized className="aspect-square"/>
-                            </AvatarImage>
-                            <AvatarFallback>
-                                <StoreIcon className="size-4 md:size-5 lg:size-6 text-primary/50 group-hover:text-primary transition-all" />
-                            </AvatarFallback>
-                        </Avatar>
-                        <h2 className="text-lg md:text-xl lg:text-2xl font-bold shrink-0">{store.name}</h2>
-                    </CardTitle>
-                </CardHeader>
-                <CardContent>
-                    <p className="text-sm text-muted-foreground">{store.description || t("no-description")}</p>
-                </CardContent>
-                <CardFooter className="justify-between items-center gap-2">
-                    <p className="text-sm text-muted-foreground items-center gap-2 hidden md:flex">
-                        <Calendar className="size-4" />
-                        {store.created_at.toLocaleDateString()}
-                    </p>
-                    <p className="text-md">
-                        {store._count?.products || 0} {t("products")}
-                    </p>
-                </CardFooter>
-            </Card>
-        </Link>
+        <Card className="transition-all gap-2 md:gap-3 lg:gap-4 shrink-0 group h-full group relative">
+            <CardHeader className="gap-0 items-center">
+                <CardTitle className="flex items-start md:items-center gap-2 truncate">
+                    <Avatar className="aspect-square size-8 lg:size-10 shrink-0">
+                        <AvatarImage src={store.logo || ""} alt={store.name} asChild className="aspect-square">
+                            <Image src={store.logo || ""} alt={store.name} width={32} height={32} unoptimized className="aspect-square" />
+                        </AvatarImage>
+                        <AvatarFallback>
+                            <StoreIcon className="size-4 md:size-5 lg:size-6 text-primary/50 group-hover:text-primary transition-all" />
+                        </AvatarFallback>
+                    </Avatar>
+                    <div className="flex flex-col truncate">
+                        <Link href={`/stores/${store.slug}/account`} className="cursor-pointer hover:text-primary transition-colors leading-0 truncate">
+                            <h2 className="text-lg md:text-xl lg:text-2xl font-bold shrink-0 leading-6 truncate">{store.name}</h2>
+                        </Link>
+                        <CardDescription className="truncate">
+                            <p className="text-sm text-muted-foreground truncate">
+                                {store.description || t("no-description")}
+                            </p>
+                        </CardDescription>
+                    </div>
+                </CardTitle>
+                <CardAction className="shrink-0">
+                    <DropDrawer>
+                        <DropDrawerTrigger>
+                            <IconButton
+                                className="shrink-0"
+                                icon={EllipsisVertical}
+                            />
+                        </DropDrawerTrigger>
+                        <DropDrawerContent>
+
+                        </DropDrawerContent>
+                    </DropDrawer>
+                </CardAction>
+            </CardHeader>
+            <CardFooter className="justify-between items-center gap-2">
+                <p className="text-sm text-muted-foreground items-center gap-2 hidden md:flex">
+                    <Calendar className="size-4" />
+                    {store.created_at.toLocaleDateString()}
+                </p>
+                <p className="text-md">
+                    {store._count?.products || 0} {t("products")}
+                </p>
+            </CardFooter>
+        </Card>
     )
 }
 
