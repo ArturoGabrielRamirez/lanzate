d60a2a9d96e8c5507bc59076e0f4e9fa83a56132
Horacio Gutierrez <horacio.estevez@gmail.com>
2025-11-21 20:17:58 -0300

    refactor: simplify TabLayout component by removing commented-out code and enhancing translation handling for better clarity

--- app/[locale]/stores/[slug]/[tab]/layout.tsx
+++ app/[locale]/stores/[slug]/[tab]/layout.tsx
@@ -1,7 +1,5 @@
-/* import { BookOpenText, Building2, UsersRound, Box, Store, ShoppingCart, Settings, ChartLine, Clock, Paintbrush } from "lucide-react" */
 import { getTranslations } from "next-intl/server"
 
-/* import { PageHeader } from "@/features/dashboard/components" */
 import { SectionContainer } from "@/features/stores/components"
 import { TabLayoutProps } from "@/features/stores/types"
 
@@ -10,18 +8,6 @@ async function TabLayout({ children, params }: TabLayoutProps) {
     const { tab } = await params
     const t = await getTranslations("store.tabs")
 
-    /*     const titles = {
-            overview: <><BookOpenText className="size-8" />{t("overview")}</>,
-            account: <><Store className="size-8" />{t("account")}</>,
-            branches: <><Building2 className="size-8" />{t("branches")}</>,
-            employees: <><UsersRound className="size-8" />{t("employees")}</>,
-            products: <><Box className="size-8" />{t("products")}</>,
-            orders: <><ShoppingCart className="size-8" />{t("orders")}</>,
-            settings: <><Settings className="size-8" />{t("settings")}</>,
-            analytics: <><ChartLine className="size-8" />{t("analytics")}</>,
-            history: <><Clock className="size-8" />{t("history")}</>,
-            styles: <><Paintbrush className="size-8" />{t("styles")}</>,
-        } */
     const titles = {
         overview: t("overview"),
         account: t("account"),
