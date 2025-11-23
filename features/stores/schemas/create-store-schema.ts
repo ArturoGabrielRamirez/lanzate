e3b851b96b4b62278696bc0f9f24e607ae284454
Horacio Gutierrez <horacio.estevez@gmail.com>
2025-11-23 01:03:43 -0300

    refactor: simplify contact info schema by removing redundant validation rules for phone, email, and social media fields

--- features/stores/schemas/create-store-schema.ts
+++ features/stores/schemas/create-store-schema.ts
@@ -44,29 +44,24 @@ export const addressInfoSchema = yup.object({
 
 export const contactInfoSchema = yup.object({
     contact_info: yup.object({
-        contact_phone: yup.string().max(20, "El número de teléfono debe tener menos de 20 caracteres").required("El número de teléfono es obligatorio"),
-        contact_email: yup.string().email("Formato de correo electrónico inválido").max(255, "El correo electrónico debe tener menos de 255 caracteres").required("El correo electrónico es obligatorio"),
-        facebook_url: yup.string().url("Debe ser una URL válida").max(255, "La URL de Facebook debe tener menos de 255 caracteres"),
-        instagram_url: yup.string().url("Debe ser una URL válida").max(255, "La URL de Instagram debe tener menos de 255 caracteres"),
-        x_url: yup.string().url("Debe ser una URL válida").max(255, "La URL de X debe tener menos de 255 caracteres"),
         phones: yup.array().of(
             yup.object({
                 phone: yup.string().max(20, "El número de teléfono debe tener menos de 20 caracteres").required("El número de teléfono es obligatorio").matches(/^[0-9+\-\s\(\)]*$/, "Debe ser un número telefónico válido"),
                 is_primary: yup.boolean().default(false),
             })
-        ).min(1, "Al menos un teléfono es obligatorio"),
+        ),
         emails: yup.array().of(
             yup.object({
                 email: yup.string().email("Formato de correo electrónico inválido").max(255, "El correo electrónico debe tener menos de 255 caracteres").required("El correo electrónico es obligatorio"),
                 is_primary: yup.boolean().default(false),
             })
-        ).min(1, "Al menos un correo electrónico es obligatorio"),
+        ),
         social_media: yup.array().of(
             yup.object({
                 url: yup.string().url("Debe ser una URL válida").max(255, "La URL de la red social debe tener menos de 255 caracteres").required("La URL de la red social es obligatoria"),
                 is_primary: yup.boolean().default(false),
             })
-        ).min(1, "Al menos una red social es obligatoria"),
+        ),
     })
 })
 
