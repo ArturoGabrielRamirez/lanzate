42158b0c1922eb64e5f35656fe3546c971014850
Horacio Gutierrez <horacio.estevez@gmail.com>
2025-11-26 22:20:12 -0300

    feat: add @base-ui-components/react and related dependencies, implement Tabs component in account management, and update layout for improved UI structure

--- app/[locale]/stores/[slug]/layout.tsx
+++ app/[locale]/stores/[slug]/layout.tsx
@@ -4,11 +4,13 @@ import { HelpCard } from "@/features/dashboard/components"
 import { getUserInfo } from "@/features/global/actions"
 import { PageContainer } from "@/features/layout/components"
 import { Item, ItemContent, ItemGroup, ItemMedia, ItemTitle } from "@/features/shadcn/components/item"
+import { Tabs, TabsList, TabsPanel, TabsTab } from "@/features/shadcn/components/ui/tabs"
 import { getStoreBasicsBySlugAction } from "@/features/stores/actions"
 import { SectionContainer, StoreHeaderServer, StoreHeaderSkeleton, StoreHeaderTinyWidgets } from "@/features/stores/components"
 import { STORES_NAVIGATION_LINKS } from "@/features/stores/constants"
 import { StoreDetailsLayoutProps } from "@/features/stores/types"
 import { Link, redirect } from "@/i18n/naviation"
+
 
 export async function generateMetadata({ params }: { params: Promise<{ slug: string }> }) {
 
@@ -60,21 +62,31 @@ async function StoreDetailsLayout({ children, params }: StoreDetailsLayoutProps)
                     {children}
                 </div>
                 <div className="hidden lg:grid lg:grid-cols-[1fr_2fr] lg:gap-8">
+                    {/* <Tabs>
+                        <TabsList variant="underline">
+                            <TabsTab value="tab-1">Tab 1</TabsTab>
+                            <TabsTab value="tab-2">Tab 2</TabsTab>
+                            <TabsTab value="danger-zone">Danger zone</TabsTab>
+                        </TabsList>
+                        <TabsPanel value="tab-1">
+                            {children}
+                        </TabsPanel>
+                    </Tabs> */}
                     <div className="flex flex-col gap-8 sticky top-24">
-                        <SectionContainer title="Tu resumen">
+                        {/* <SectionContainer title="Tu resumen">
                             <StoreHeaderTinyWidgets slug={slug} />
-                        </SectionContainer>
+                        </SectionContainer> */}
                         <SectionContainer title="Tus atajos" className="@container">
-                            <ItemGroup className="grid @md:grid-cols-4 grid-cols-3 gap-4">
+                            <ItemGroup className="grid @md:grid-cols-6 grid-cols-3 gap-4">
                                 {STORES_NAVIGATION_LINKS.map((link) => (
-                                    <Item key={link.href} variant="outline" className="flex-col items-center p-2 @md:p-4 group/item gap-2 @lg:gap-4" asChild>
+                                    <Item key={link.href} variant="outline" className="p-2 group/item gap-2 @lg:gap-4 truncate aspect-square" asChild>
                                         <Link href={`/stores/${slug}${link.href}`}>
-                                            <ItemMedia variant="icon" className="mx-auto size-6 bg-transparent border-none text-muted-foreground group-hover/item:text-primary group-hover/item:scale-125 transition-all duration-100">
+                                            <ItemMedia variant="icon" className="mx-auto size-6 bg-transparent border-none text-muted-foreground group-hover/item:text-primary group-hover/item:scale-125 transition-all duration-100 self-center">
                                                 {link.icon}
                                             </ItemMedia>
-                                            <ItemContent>
-                                                <ItemTitle className="font-medium text-xs md:text-sm text-muted-foreground group-hover/item:text-foreground">{link.label}</ItemTitle>
-                                            </ItemContent>
+                                            {/* <ItemContent className="truncate">
+                                                <ItemTitle className="font-medium text-xs text-muted-foreground group-hover/item:text-foreground truncate">{link.label}</ItemTitle>
+                                            </ItemContent> */}
                                         </Link>
                                     </Item>
                                 ))}
@@ -82,9 +94,7 @@ async function StoreDetailsLayout({ children, params }: StoreDetailsLayoutProps)
                         </SectionContainer>
                         <HelpCard />
                     </div>
-                    <div className="flex flex-col gap-8">
-                        {children}
-                    </div>
+                    {children}
                 </div>
             </div>
         </PageContainer>
