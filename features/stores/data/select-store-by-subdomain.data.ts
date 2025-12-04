84b22cbd51baa10d39f1a76dd6625ffee50935f6
ArturoGabrielRamirez <jiamafox@gmail.com>
2025-12-04 19:06:38 -0300

    data commented at dfeatures/store

--- features/stores/data/select-store-by-subdomain.data.ts
+++ features/stores/data/select-store-by-subdomain.data.ts
@@ -1,32 +1,34 @@
 "use server"
 
-import { prisma } from "@/utils/prisma";
+/* import { prisma } from "@/utils/prisma"; */
 
 export async function selectStoreBySubdomainData(subdomain: string) {
-    const store = await prisma.store.findUnique({
-        where: {
-            subdomain: subdomain
-        },
-        include: {
-            branches: true,
-            products: {
-                where: {
-                    is_deleted: false
-                },
-                include: {
-                    categories: true,
-                    stock_entries: true
-                }
-            },
-            balance: true,
-           /*  operational_settings: true */
-           // TODO: Arreglar aca, para Hori
-        }
-    })
+
+    console.log("arreglar o depurar selectStoreBySubdomainData", subdomain)
+    /*    const store = await prisma.store.findUnique({
+           where: {
+               subdomain: subdomain
+           },
+           include: {
+               branches: true,
+               products: {
+                   where: {
+                       is_deleted: false
+                   },
+                   include: {
+                       categories: true,
+                       stock_entries: true
+                   }
+               },
+               balance: true, */
+    /*  operational_settings: true */
+    // TODO: Arreglar aca, para Hori
+    /*      }
+     }) */
 
     return {
         message: "Tienda recuperada con éxito desde la base de datos",
-        payload: store,
+        payload: null/* store */,
         hasError: false
     }
 }
